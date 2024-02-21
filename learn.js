42cd4208e86aefb3c7edf8245aced2bdfa40e6af
ha00xD <haohaoxdm19@gmail.com>
2024-02-21 13:18:28 +0630

    about add

---
+++ learn.js
@@ -0,0 +1,83 @@
+// function findAge(birthYear) {
+//   let currentYear = 2024;
+//   let age = currentYear - birthYear;
+//   return age;
+// }
+// console.log(findAge(2004));
+
+// ********************************************************************
+
+// console.log("Shopping Recipt");
+
+// let total = 0;
+
+// function buy(productName, productPrice, quantity) {
+//   let cash = productPrice * quantity;
+//   total += cash;
+//   return productName + "---" + productPrice + "---" + cash;
+// }
+// function findTax(amount, percentage) {
+//   return amount * (percentage / 100);
+// }
+
+// console.log(buy("Durain", 7000, 2));
+// console.log(buy("Mango", 700, 5));
+// console.log(buy("Avacado", 1000, 1));
+// console.log(buy("Banana", 500, 3));
+// console.log(buy("Strawberry", 100, 10));
+// console.log("Total --- " + total);
+// let tax = findTax(total, 5);
+// console.log("Tax --- " + tax);
+// console.log("Cash ---" + (total + tax));
+// ********************************************************************
+
+// Conditional Statement
+
+// function findEvenOdd(number){
+//     let reminder = number%2
+//     if( reminder == "0"){
+//         return "Even"
+//     }
+//     else{
+//         return "Odd"
+//     }
+// }
+// console.log(findEvenOdd("3"));
+// console.log(findEvenOdd("2"));
+// console.log(findEvenOdd("4"));
+// console.log(findEvenOdd("7"));
+
+
+// **************************************************
+// let result = "Over All Passed"
+
+// function findPassaFail(marks){
+//     if(marks>=40){
+//         return "Passed"
+//     }
+//     else{
+//         result = "Over All Failed"
+//         return "Failed"
+//     }
+// }
+// console.log(findPassaFail(39));
+// console.log(findPassaFail(45));
+// console.log(findPassaFail(40));
+// console.log(findPassaFail(29));
+// console.log(result);
+
+// ********************************************************************
+// Array,Obj
+  
+// ********************************************************************
+
+
+// Looping
+
+// for(let i=0;i<=100;i+=10){
+//    console.log(i);
+// }
+
+// for (let i=100;i>=0;i-=10){
+//     console.log(i);
+// }
